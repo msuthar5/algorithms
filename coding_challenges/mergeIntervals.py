cab4669f31dad6a3f5efa60e015227c3c6736ce0
msuthar5 <msuthar@hawk.iit.edu>
2019-11-27 13:13:15 -0600

    quick change

--- coding_challenges/mergeIntervals.py
+++ coding_challenges/mergeIntervals.py
@@ -5,7 +5,7 @@ def merge(intervals: List[List[int]]) -> List[List[int]]:
     intervals.sort()
     i=1
     while i < len(intervals):
-        if intervals[i-1][0]<=intervals[i][0]<=intervals[i-1][1]:
+        if intervals[i][0]<=intervals[i-1][1]:
             intervals[i-1][1]=max(intervals[i-1][1],intervals[i][1])
             intervals.pop(i)
         else:
