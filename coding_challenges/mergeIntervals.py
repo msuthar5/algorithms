350b2441b4fb697e23351693af24c26c5eba4c57
msuthar5 <msuthar@hawk.iit.edu>
2019-11-27 13:03:09 -0600

    leetcode #56

---
+++ coding_challenges/mergeIntervals.py
@@ -0,0 +1,13 @@
+"""
+Leetcode #56: Merge Intervals
+"""
+def merge(intervals: List[List[int]]) -> List[List[int]]:
+    intervals.sort()
+    i=1
+    while i < len(intervals):
+        if intervals[i-1][0]<=intervals[i][0]<=intervals[i-1][1]:
+            intervals[i-1][1]=max(intervals[i-1][1],intervals[i][1])
+            intervals.pop(i)
+        else:
+            i+=1
+    return intervals
